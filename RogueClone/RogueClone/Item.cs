6812fd8cc7baec169bfa3dda027d1f6f4fe7b7a8
jumperabg <krasimirvelichkov@gmail.com>
2015-03-24 23:48:43 +0200

    Basic Commit

---
+++ RogueClone/RogueClone/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RogueClone
+{
+    public class Item
+    {
+
+        public int Durability
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int NeededLvl
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
