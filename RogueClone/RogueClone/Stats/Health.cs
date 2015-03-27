1bcfeec86cabfbd4fc2d0666d8d780d2a19033d6
Pariador <pariador.corterian@gmail.com>
2015-03-27 15:51:01 +0200

    Stuff2

---
+++ RogueClone/RogueClone/Stats/Health.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RogueClone
+{
+    public class Health
+    {
+        public int Max
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Current
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void Increase()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
