a2344cad1786ba195f06e486115a736c906961d1
Gergana Belcheva <gergana.belcheva@gmail.com>
2015-04-03 17:32:44 +0300

    health potion constructor

--- RogueClone/RogueClone/Items/Consumables/HealthPotion.cs
+++ RogueClone/RogueClone/Items/Consumables/HealthPotion.cs
@@ -7,31 +7,32 @@ namespace RogueClone
 {
     public class HealthPotion : Potion
     {
-        //public HealthPotion(string name, int price, int neededLevel, Point2D position, char icon, int amountRestored)
-        //    : base("Health Potion", price, neededLevel, position, '♥', amountRestored)
-        //{
-        //}
+        private const int AmountRestored = 100;
+        public HealthPotion(string name, int price, int neededLevel, Point2D position, char icon, int amountRestored)
+            : base("Health Potion", string.Format("Health +{0}", amountRestored), price, neededLevel, position, '╬', ConsoleColor.Red, amountRestored)
+        {
+        }
         public HealthPotion(Point2D position)
-            : base(position)
+            : base("Health Potion", string.Format("Health +{0}", AmountRestored), 150, 2, position, '╬', ConsoleColor.Red, AmountRestored)
         {
         }
 
-        public override string Name { get { return "Health Potion"; } }
-        public override string Description
-        {
-            get
-            {
-                return string.Format("Health +{0}", this.AmountStatsRestored);
-            }
-        }
+        //public override string Name { get { return "Health Potion"; } }
+        //public override string Description
+        //{
+        //    get
+        //    {
+        //        return string.Format("Health +{0}", this.AmountStatsRestored);
+        //    }
+        //}
 
-        public override int Value { get { return 150; } }
+        //public override int Value { get { return 150; } }
 
-        public override int NeededLvl { get { return 2; } }
+        //public override int NeededLvl { get { return 2; } }
 
-        public override char Icon { get { return '╬'; } }
-        public override ConsoleColor Color { get { return ConsoleColor.Red; } }
-        public override int AmountStatsRestored { get { return 100; } }
+        //public override char Icon { get { return '╬'; } }
+        //public override ConsoleColor Color { get { return ConsoleColor.Red; } }
+        //public override int AmountStatsRestored { get { return 100; } }
 
         public override void Consumed(Hero hero)
         {
