95cbc0c8b9d28708ded46cc5d50ce76ed02704a4
Nikolay Karagyozov <newmast@abv.bg>
2015-04-08 03:37:55 +0300

    minor text modifications

--- RogueClone/RogueClone/UI/StartMenu/GlobalMenuOptions.cs
+++ RogueClone/RogueClone/UI/StartMenu/GlobalMenuOptions.cs
@@ -8,13 +8,13 @@
         public const string StartGame = "Start New Game";
         public const string Options = "Options";
         public const string Credits = "Credits";
-        public const string Quit = "Exit";
+        public const string Quit = "Quit";
         //Start game options
         public const string WishMessage = "Good luck!";
         //Options menu options
         public const string TweakSomething = "Tweak something";
         public const string TweakSomethingElse = "Tweak something else";
-        public const string ChangeAnOption = "ChangeAnOption";
+        public const string ChangeAnOption = "Change an option";
         //Credits
         public const string ReasonForCreation = "Telerik Academy OOP Teamwork 2015";
         public const string TeamName = "TEAM WATERMELON";
