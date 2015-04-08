4b8503da498642231275ec9e405f2ada90f315a5
Pariador <pariador.corterian@gmail.com>
2015-04-08 12:27:59 +0300

    FROM NIKOLAY! Added Nikolay's MainMenu.

--- RogueClone/RogueClone/GameMain.cs
+++ RogueClone/RogueClone/GameMain.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using RogueClone.UI.StartMenu;
 
 namespace RogueClone
 {
@@ -10,6 +13,27 @@ namespace RogueClone
     {
         static void Main(string[] args)
         {
+            #region MENU
+            Menu mainMenu = new MainMenu();
+
+            while (true)
+            {
+                mainMenu.PrintOnScreen();
+                var menu = mainMenu.HandleMenuInput();
+                if (menu != null)
+                {
+                    if (menu is StartGame)
+                    {
+                        menu.PrintOnScreen();
+                        Thread.Sleep(1000);
+                        break;
+                    }
+                    mainMenu = menu;
+                }
+                Thread.Sleep(50);
+            }
+            #endregion
+
             var game = RogueEngine.Instance; // Dont change the values ! They could be magical !
             game.Start();
 
